6195a44c6083e51b9bc1906c1bd4e6197000d10d
toto <toto@gmail.com>
2020-08-18 19:58:26 +0200

    adding previews

--- trusty_monkey/frontend/src/components/ReviewEditor.vue
+++ trusty_monkey/frontend/src/components/ReviewEditor.vue
@@ -1,31 +1,52 @@
 <template>
+<div>
+
   <div>
     <div style="height:200px">
-      <h3>Pics preview</h3>
+      <ReviewDetail
+          :id= "this.id"/>
     </div>
     <hr/>
+  </div>
 
-    <div >    
-      <div class="d-flex flex-row">
-      <div v-show="upUrl != null" class="custom-file">
-        <input type="file" class="custom-file-input" @change="onFileSelected">
-        <label class="custom-file-label" for="customFile">Choose file</label>
-      </div>
-      <button v-if="showPost= true" class="btn btn-sm btn-danger" @click="onUpload"> Soumettre </button>       
-      </div>
+    <div>    
       
-      <div class="d-flex flex-row justify-content-around">
-        <div class="mt-3" v-for="(buttonPic, index) in buttonPics" :key="buttonPic">
-          <button class="btn btn-sm btn-outline-success" @click="upUrl= index"> {{ buttonPic }} </button>
-          
+      <div v-show="upUrl != null && submit == false" class="custom-file">
+        <div class="d-flex flex-row">
+          <input type="file" class="custom-file-input" @change="onFileSelected">
+          <label class="custom-file-label" for="customFile">Choose file</label>
+        </div>         
+      </div>
+
+      <div v-show="submit== true" >
+        <div class="d-flex flex-row justify-content-around">
+          <button class="btn btn-success" @click="onUpload"> Soumettre </button>
+          <button class="btn btn-danger" @click="showCatBut= true, submit= false, upUrl= null"> Annuler </button>
         </div>
-      </div>       
+      </div>
+
+      <div v-show="showCatBut== true">
+        <div class="d-flex flex-row justify-content-around">
+          <div class="mt-3" v-for="(buttonPic, index) in buttonPics" :key="buttonPic">
+            <button class="btn btn-sm btn-outline-success" @click="upUrl= index, showCatBut= false"> {{ buttonPic }} </button>          
+          </div>
+        </div>
+         <div class='mt-3'>          
+            <p><i>(Plus que {{ totalPics }} pour términer!)</i></p>          
+        </div>
+      </div>
+
+      <div v-show="totalPics == 0">
+            <h1> Submit button </h1>
+      </div>  
+
     </div>
 
-  </div>
+</div> 
 </template>
 
 <script>
+import ReviewDetail from "@/components/ReviewDetail.vue";
 import axios from 'axios'
 import { CSRF_TOKEN } from "../common/csrf_token.js"
 export default {
@@ -34,9 +55,11 @@ export default {
     return {
       selectedFile: null,
       upUrl: null,
+      submit: false,
+      showCatBut: true,
       endpoint: null,
-      showPost: false,      
-      buttonPics: ['Entrée', 'Plat-principal', 'Dessert', 'menu', 'Extérieur', 'Intérieur'],              
+      totalPics: 6,           
+      buttonPics: ['Entrée', 'Plat-principal', 'Dessert', 'menu', 'Extérieur', 'Intérieur'],                     
     }
   },
   props: {
@@ -45,15 +68,16 @@ export default {
       required: true
     },
   },  
-  methods: {
+  methods: {   
     onFileSelected(event) {
-      this.selectedFile = event.target.files[0]      
+      this.selectedFile = event.target.files[0]
+      this.submit = true                 
     },
     onUpload() {      
       const fd = new FormData();
       let axiosConfig = {
         headers: {
-            'X-CSRFTOKEN': CSRF_TOKEN,           
+          'X-CSRFTOKEN': CSRF_TOKEN,           
         }
       };
       if (this.upUrl == 0) {
@@ -75,9 +99,24 @@ export default {
         .then(res => {
           console.log(res)
           this.upUrl = null
+          this.submit = false
+          this.showCatBut = true
+          this.totalPics -= 1
         })
     },   
-  },   
+  },
+  watch: {
+    totalPics: function() {
+      if (this.totalPics == 0) {     
+        this.upUrl= null,
+        this.submit= false,
+        this.showCatBut= false        
+      }
+    }
+  },
+  components: {
+    ReviewDetail,    
+   }, 
 }
 </script>
 
