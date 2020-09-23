a5fe1c493ee8b77871fa154f00b6f9280579b706
toto <toto@gmail.com>
2020-09-23 16:40:02 +0200

    Procfile added

--- trusty_monkey/trusty_monkey/settings.py
+++ trusty_monkey/trusty_monkey/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-jc7uv=+k#uv&ww&9^z@(!=49#rn7bxipp%2uagwjvcdkc&u19'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
